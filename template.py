1aedfadf7d78844b16d8367485634c1cd8a8bc84
Krajzys <kryzyyspl@gmail.com>
2021-12-08 23:07:30 +0100

    Update template.py to support readling from stdin

--- template.py
+++ template.py
@@ -4,8 +4,15 @@ def main():
     if len(argv) < 2:
         print(f"Usage: {argv[0]} <file_name>")
         exit(1)
-    filename = argv[1]
-    line_list = [line for line in open(filename)]
+    line_list = []
+    if argv[1] == '-':
+        while (line := input()) != '':
+            line_list.append(line)
+    else:
+        filename = argv[1]
+        line_list = [line.strip() for line in open(filename)]
+
+    print(line_list)
 
 
 if __name__ == "__main__":
