1a67809fb83e6933559616ef819d31d28738914d
Krajzys <kryzyyspl@gmail.com>
2021-12-09 19:13:16 +0100

    Add task from day9

--- day9/2solve.py
+++ day9/2solve.py
@@ -1,4 +1,43 @@
 from sys import argv
+from functools import reduce
+
+def print_map(map):
+    for r in map:
+            for v in r:
+                print(f"{v}", end="")
+            print()
+
+
+def get_basin_sizes(basin_map):
+    basin_sizes = {}
+    for r in basin_map:
+        for c in r:
+            if c == 'X':
+                continue
+            if c in basin_sizes.keys():
+                basin_sizes[c] += 1
+            else:
+                basin_sizes[c] = 1
+
+    return basin_sizes
+
+
+def claim_basin_within(basin_number, point, basin_map):
+    r, c = point
+    if r < 0 or r >= len(basin_map) or c < 0 or c >= len(basin_map[0]) or basin_map[r][c] != 0:
+        return
+    basin_map[r][c] = basin_number
+    claim_basin_within(basin_number, (r-1, c), basin_map)
+    claim_basin_within(basin_number, (r+1, c), basin_map)
+    claim_basin_within(basin_number, (r, c-1), basin_map)
+    claim_basin_within(basin_number, (r, c+1), basin_map)
+
+def map_basins(low_points, basin_map):
+    for r, c in low_points:
+        basin_number = basin_map[r][c]
+        basin_map[r][c] = 0
+        claim_basin_within(basin_number, (r, c), basin_map)
+
 
 def check_low_point(r, c, height_map):
     neighbors_count = 0
@@ -42,21 +81,25 @@ def main():
 
     basin_map = [r[:] for r in height_map]
     low_point_count = 1
+    low_points = []
     for i, r in enumerate(height_map):
         for j, _ in enumerate(r):
             if check_low_point(i, j, height_map):
+                low_points.append((i, j))
                 basin_map[i][j] = low_point_count
                 low_point_count += 1
             else:
                 if height_map[i][j] == 9:
-                    basin_map[i][j] = 9
+                    basin_map[i][j] = 'X'
                 else:
                     basin_map[i][j] = 0
 
-    for r in basin_map:
-        for v in r:
-            print(f"{v}", end="")
-        print()
+    map_basins(low_points, basin_map)
+    basin_sizes = get_basin_sizes(basin_map)
+    three_biggest_areas = sorted(basin_sizes.values())[-3:]
+
+    print(three_biggest_areas)
+    print(reduce(lambda v, acc: acc * v, three_biggest_areas, 1))
 
 
 if __name__ == "__main__":
