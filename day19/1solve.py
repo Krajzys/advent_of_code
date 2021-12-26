e144182c06e93892ac0948b98a15daa6445f1c76
Krajzys <kryzyyspl@gmail.com>
2021-12-27 00:52:41 +0100

    Every day we wake up

--- day19/1solve.py
+++ day19/1solve.py
@@ -1,8 +1,4 @@
 from sys import argv
-from statistics import mean, median_grouped
-from math import inf
-from time import time
-from typing import ChainMap, Text
 from progress import Progress
 import data
 
@@ -49,6 +45,7 @@ def get_intersect_len(list1, list2):
                 intersect += 1
     return intersect
 
+
 def main():
     if len(argv) < 2:
         print(f"Usage: {argv[0]} <file_name>")
@@ -85,7 +82,8 @@ def main():
     overlaps = dict()
     if not import_data:
         count = 1
-        progress = Progress(0, len(in_scanner_distances)**2 - len(in_scanner_distances))
+        progress = Progress(0, len(in_scanner_distances) **
+                            2 - len(in_scanner_distances))
         print('Determining overlapping scanners...')
         for scanner_no, poss_dist in in_scanner_distances.items():
             for scanner_no_2, poss_dist2 in in_scanner_distances.items():
@@ -94,10 +92,12 @@ def main():
                     continue
                 for from_no, dist in poss_dist.items():
                     for from_no2, dist2 in poss_dist2.items():
-                        intersect_len = get_intersect_len(dist.values(), dist2.values())
+                        intersect_len = get_intersect_len(
+                            dist.values(), dist2.values())
                         if intersect_len >= 11:
                             if (scanner_no, scanner_no_2) not in overlaps and (scanner_no_2, scanner_no) not in overlaps:
-                                overlaps[(scanner_no, scanner_no_2)] = (from_no, from_no2)
+                                overlaps[(scanner_no, scanner_no_2)] = (
+                                    from_no, from_no2)
                 progress.update(count)
                 count += 1
 
@@ -125,17 +125,19 @@ def main():
         for k, v in beacons.items():
             for k2, v2 in beacons2.items():
                 if is_same_b2b_dist(v, v2):
-                    if (s1, k) in same_beacons:
-                        same_beacons[(s1, k)].add((s2, k2))
-                    else:
+                    if (s1, k) not in same_beacons:
                         same_beacons[(s1, k)] = set()
-                        same_beacons[(s1, k)].add((s2, k2))
+                    same_beacons[(s1, k)].add((s2, k2))
+
+    print(f"Same beacons ({len(same_beacons)}):")
+    for k, v in same_beacons.items():
+        print(k, v)
 
     merged = {}
     for k, v in same_beacons.items():
         merged[k] = v.copy()
 
-    # Phase 1
+    # Phase 1 - Reduce keys that are also in values
     # for every key and values:
     #   for every value in values:
     #     if value in all_keys:
@@ -148,8 +150,25 @@ def main():
     #      (3,4): {(1,2),(1,3),(2,3)}
     # }
     # dict{(3,4): {(1,2),(1,3),(2,3),(1,3)}}
-    #
-    # Phase 2
+
+    popped = set()
+    for k, vals in merged.items():
+        new_vals = vals.copy()
+        for v in vals:
+            if v in merged and v not in popped:
+                new_vals = new_vals.union(merged[v])
+                popped.add(v)
+        merged[k] = new_vals
+
+    for i in popped:
+        merged.pop(i)
+
+    print(f"Popped: {popped}")
+    print(f"Merged P1 ({len(merged)}):")
+    for k, v in merged.items():
+        print(k, v)
+
+    # Phase 2 - Match beacons if the overlapping beacon only appears in values
     # For every key and values:
     #   For every value in values:
     #     For every key2 and values2:
@@ -161,14 +180,46 @@ def main():
     #      (1,3): {(2,3),(3,4),(4,5)}}
     # dict{(1,2): {(2,3),(1,3),(2,3),(3,4),(4,5)}}
 
+    popped.clear()
+    checked = set()
+    for k, vals in merged.items():
+        new_vals = vals.copy()
+        for v in vals:
+            for k2, vals2 in merged.items():
+                if k == k2:
+                    continue
+                if v in vals2 and k2 not in popped:
+                    if k2 not in checked:
+                        popped.add(k2)
+                    new_vals = new_vals.union(merged[k2])
+                    new_vals.add(k2)
+        checked.add(k)
+        merged[k] = new_vals
+
+    print("Popping:")
+    for i in popped:
+        print(i, merged[i])
+        merged.pop(i)
+
+    all_vals = set()
+    for k, v in merged.items():
+        all_vals = all_vals.union(v)
+
+    print(f"Popped: {popped}")
+    print(f"Merged P2 ({len(merged)}):")
+    for k, v in merged.items():
+        print(k, v)
+
+    print(len(uniq_beacons))
+
+    # Remove overlapping beacons (leaves only the beacon that is the key in the dictionary)
     for (s, b), non_uniques in merged.items():
         for n in non_uniques:
             if (s, b) == n:
                 continue
-            if n in uniq_beacons:
-                uniq_beacons.remove(n)
+            uniq_beacons.remove(n)
 
-    print(len(uniq_beacons))
+    print(len(uniq_beacons), len(all_vals))
 
 
 if __name__ == "__main__":
